3a01c026e726c67fd72971364f1bc8e861d61637
Zhenyu Zhou <787599881@qq.com>
2025-12-02 10:58:46 +0800

    fix l2 reward fee calculation

--- src/L2/core/L2RewardManager.sol
+++ src/L2/core/L2RewardManager.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {L2RewardManagerStorage} from "./L2RewardManagerStorage.sol";
@@ -16,15 +15,13 @@ contract L2RewardManager is L2Base, L2RewardManagerStorage {
     /// @notice 质押者收益占比(默认 92%)
     uint256 public stakerPercent = 92;
 
-    constructor(){
+    constructor() {
         _disableInitializers();
     }
 
     /// @notice 初始化 L2 奖励管理合约
     /// @param initialOwner 初始所有者地址
-    function initialize(
-        address initialOwner
-    ) external initializer {
+    function initialize(address initialOwner) external initializer {
         __L2Base_init(initialOwner);
     }
 
@@ -43,25 +40,20 @@ contract L2RewardManager is L2Base, L2RewardManagerStorage {
 
         // 计算质押者部分的手续费(92%)
         uint256 stakerFee = (operatorTotalFee * stakerPercent) / 100;
-        stakerRewards[strategy] += stakerFee;
+        stakerRewards[strategy] = stakerFee;
 
         // 计算运营商部分的手续费(8%)
         uint256 operatorFee = (operatorTotalFee * (100 - stakerPercent)) / 100;
-        operatorRewards[operator] += operatorFee;
+        operatorRewards[operator] = operatorFee;
 
-        emit OperatorStakerReward(
-            strategy,
-            operator,
-            stakerFee,
-            operatorFee
-        );
+        emit OperatorStakerReward(strategy, operator, stakerFee, operatorFee);
     }
 
     /// @notice 存入 DappLink 代币作为奖励
     /// @dev 任何人都可以调用此函数向奖励池充值 DappLink 代币
     /// @param amount 存入的代币数量
     /// @return 是否成功
-    function depositDappLinkToken(uint256 amount) external returns (bool){
+    function depositDappLinkToken(uint256 amount) external returns (bool) {
         getDapplinkToken().safeTransferFrom(msg.sender, address(this), amount);
         emit DepositDappLinkToken(msg.sender, amount);
         return true;
@@ -70,17 +62,14 @@ contract L2RewardManager is L2Base, L2RewardManagerStorage {
     /// @notice 运营商申领奖励
     /// @dev 运营商提取其累积的 DappLink 代币奖励
     /// @return 是否成功
-    function operatorClaimReward() external returns (bool){
+    function operatorClaimReward() external returns (bool) {
         uint256 claimAmount = operatorRewards[msg.sender];
         require(claimAmount > 0, "No rewards to claim");
         // 清零防止重复申领
         operatorRewards[msg.sender] = 0;
 
         getDapplinkToken().safeTransferFrom(address(this), msg.sender, claimAmount);
-        emit OperatorClaimReward(
-            msg.sender,
-            claimAmount
-        );
+        emit OperatorClaimReward(msg.sender, claimAmount);
         return true;
     }
 
@@ -88,15 +77,12 @@ contract L2RewardManager is L2Base, L2RewardManagerStorage {
     /// @dev 质押者提取其在指定策略中的 DappLink 代币奖励
     /// @param strategy 策略地址
     /// @return 是否成功
-    function stakerClaimReward(address strategy) external returns (bool){
+    function stakerClaimReward(address strategy) external returns (bool) {
         uint256 stakerAmount = stakerRewardsAmount(strategy);
         require(stakerAmount > 0, "No rewards to claim");
         claimedStakerRewards[strategy][msg.sender] += stakerAmount;
         getDapplinkToken().safeTransferFrom(address(this), msg.sender, stakerAmount);
-        emit StakerClaimReward(
-            msg.sender,
-            stakerAmount
-        );
+        emit StakerClaimReward(msg.sender, stakerAmount);
         return true;
     }
 
@@ -104,13 +90,13 @@ contract L2RewardManager is L2Base, L2RewardManagerStorage {
     /// @dev 根据质押者份额占策略总份额的比例计算奖励
     /// @param strategy 策略地址
     /// @return 质押者可获得的奖励金额
-    function stakerRewardsAmount(address strategy) public view returns (uint256){
+    function stakerRewardsAmount(address strategy) public view returns (uint256) {
         // 获取质押者在该策略中的份额
         uint256 stakerShare = getStrategyManager().getStakerStrategyShares(msg.sender, strategy);
         // 获取策略的总份额
         uint256 strategyShares = getStrategy(strategy).totalShares();
         // 如果质押者份额或策略总份额为 0,返回 0
-        if (stakerShare == 0 ||strategyShares == 0) {
+        if (stakerShare == 0 || strategyShares == 0) {
             return 0;
         }
         // 按比例计算奖励: 策略总奖励 * (质押者份额 / 策略总份额)
